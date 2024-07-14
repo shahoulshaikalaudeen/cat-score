9fd625c8ed00261dd84aa032a7887ee72240eb4c
shahoul95 <shahoul@libheros.fr>
2024-07-14 16:00:01 +0200

    add production url

--- src/app/services/cat.service.ts
+++ src/app/services/cat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface Cat {
   id: string;
@@ -13,7 +14,7 @@ export interface Cat {
 })
 export class CatService {
 
-  private apiUrl = 'http://localhost:5000/cats';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
